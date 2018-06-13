977bdbf58cf96db18981defaf617827f49943cb6
Kristie Desiree <kly5@mail.sfsu.edu>
2018-06-13 11:34:38 -0700

    IOCE and pseudocode for enqueue method

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -4,16 +4,36 @@ var Queue = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  //initialize a variable that contains current queueSize
+
+
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
   };
+  //input: a value
+  //output: none
+  //constraints: none
+  //edge cases: none
 
+  //queue always adds to the end (index zero)
+  //if queue size is zero
+    //add a key-value pair to storage obj where the key is queueSize and the value is value
+    //increment queueSize
+  //else (queue has items already)
+    //shift existing key-value pairs : for loop to iterate the objkeys length where i starts at one
+      //no need to start at zero because it will be vacated
+      //for each index
+        //set obj[i] to be obj[i-1]
+    //insert new key-val pair at obj['0'] (first index)
+    //increment queuesize
   someInstance.dequeue = function() {
   };
 
   someInstance.size = function() {
+  //inpt
   };
 
   return someInstance;
 };
+
